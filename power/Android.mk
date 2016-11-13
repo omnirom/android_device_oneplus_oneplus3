c4d58163f742a70dede2d2809a7f141edd715716
maxwen <max.weninger@gmail.com>
2016-11-13 01:51:20 +0100

    op3: 7.1 bringup
    power: remove obsolete stuff
    
    Change-Id: I092f072aae2cb24e18e1f27024375c7acafa772c

--- power/Android.mk
+++ power/Android.mk
@@ -6,8 +6,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
-LOCAL_SRC_FILES := power.c \
-                   util.c
+LOCAL_SRC_FILES := power.c
 
 LOCAL_MODULE := power.msm8996
 LOCAL_MODULE_TAGS := optional
