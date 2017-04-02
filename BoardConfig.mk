b2580713f27ed1b6c3870979fcd803a2cbeb59cf
maxwen <max.weninger@gmail.com>
2017-04-02 20:58:40 +0200

    op3(t): prepare for 7.1.1 blobs
    we have to start with sh.. somewhere
    thanks to https://github.com/LineageOS/android_device_oneplus_oneplus3/commit/71c60cb4da48736f8e7f3f11f96b6ea4899e8b5a
    
    Change-Id: Idf9f621daf738605ed9317b18160fdb3625141b1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,7 +48,7 @@ TARGET_USES_64_BIT_BINDER := true
 
 ENABLE_CPUSETS := true
 
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.selinux=enforcing user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff androidboot.bootdevice=624000.ufshc
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.selinux=permissive user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff androidboot.bootdevice=624000.ufshc
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x02000000
@@ -58,7 +58,7 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_APPEND_DTB := true
-TARGET_KERNEL_SOURCE := kernel/oneplus/msm8996
+TARGET_KERNEL_SOURCE := kernel/oneplus/android_kernel_oneplus_msm8996/
 TARGET_KERNEL_CONFIG := omni_oneplus3_defconfig
 
 # partitions
@@ -130,7 +130,6 @@ AUDIO_FEATURE_ENABLED_PLAYBACK_ULL := false
 # Camera
 USE_CAMERA_STUB := true
 TARGET_USES_MEDIA_EXTENSIONS := true
-TARGET_CAMERASERVICE_CLOSES_NATIVE_HANDLES := true
 
 # Disable secure discard because it's SLOW
 BOARD_SUPPRESS_SECURE_ERASE := true
