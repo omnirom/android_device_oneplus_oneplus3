a3523a9e5969bc943afe67dbcccb2059ea2d05bb
Menno van Grinsven <scanno71@gmail.com>
2017-09-10 19:37:47 +0200

    op3(t): Add NFC HAL to device make file
    Change-Id: If708272cc0839d832ec7e7196d400a3d34a99099

--- device.mk
+++ device.mk
@@ -180,6 +180,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
+# NFC HAL
+PRODUCT_PACKAGES += \
+    vendor.nxp.hardware.nfc@1.0
+
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
