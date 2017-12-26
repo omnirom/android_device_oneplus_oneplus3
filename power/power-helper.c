6f658715c3a760cbd64d8989c5e463c305be823f
maxwen <max.weninger@gmail.com>
2017-12-26 02:21:12 +0100

    op3(t): power: add property to disable touch boost
    add ro.vendor.power.touchboost_enabled=false and reboot
    
    Change-Id: I88b996060cd06ecad40f6bee5c836b1f03e9317e

--- power/power-helper.c
+++ power/power-helper.c
@@ -109,6 +109,7 @@ int vr_mode = 0;
 //interaction boost global variables
 static struct timespec s_previous_boost_timespec;
 static int s_previous_duration;
+static bool is_touchboost_enabled;
 
 void power_init(void)
 {
@@ -129,6 +130,8 @@ void power_init(void)
         }
         close(fd);
     }
+    is_touchboost_enabled = get_touchboost_enabled();
+    ALOGI("is_touchboost_enabled = %d", is_touchboost_enabled);
 }
 
 static void process_video_decode_hint(void *metadata)
@@ -409,6 +412,10 @@ void power_hint(power_hint_t hint, void *data)
         case POWER_HINT_INTERACTION:
         {
             char governor[80];
+
+            if (!is_touchboost_enabled) {
+                return;
+            }
 
             if (get_scaling_governor(governor, sizeof(governor)) == -1) {
                 ALOGE("Can't obtain scaling governor.");
